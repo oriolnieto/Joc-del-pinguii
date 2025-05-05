833b13399190b1d2d6493077701c47a843dca178
LILLK <oriolnieto2006@gmail.com>
2025-05-05 16:22:15 +0200

    added atras button

--- src/vista/pantallaRegistroController.java
+++ src/vista/pantallaRegistroController.java
@@ -1,9 +1,16 @@
 package vista;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -16,6 +23,14 @@ public class pantallaRegistroController {
     @FXML
     private PasswordField passField;
 
+    @FXML
+    public void switchAtras(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/pantallaLogin.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.setTitle("Joc del Pingüí");
+        stage.show();
+    }
     @FXML
     private void handleRegister() {
         String usuario = userField.getText();
