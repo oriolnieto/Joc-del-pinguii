3cb27cd9def487f61dc1b5a4276b8c6d27d6a894
LILLK <oriolnieto2006@gmail.com>
2025-05-05 16:24:00 +0200

    modified salir button

--- src/vista/pantallaMenuController.java
+++ src/vista/pantallaMenuController.java
@@ -11,9 +11,14 @@ import javafx.stage.Stage;
 
 public class pantallaMenuController {
     
-    public void botoSortir() {
-        Platform.exit();
-    }
+	 public void botoSortir(ActionEvent event) throws IOException {
+
+         Parent root = FXMLLoader.load(getClass().getResource("/pantallaLogin.fxml"));
+         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+         stage.setScene(new Scene(root));
+         stage.setTitle("Joc del Pingüí");
+         stage.show();
+   }
 
     public void botoNovaPartida(ActionEvent event) throws IOException {
 
