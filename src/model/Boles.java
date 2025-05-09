6d3cc6289014a98140e768f2ed22fd84f6e1fef2
alex24050714 <alexmontserrat2405a@alumnes.ilerna.com>
2025-05-09 16:48:37 +0200

    boles de neu

--- src/model/Boles.java
+++ src/model/Boles.java
@@ -12,7 +12,11 @@ public class Boles extends Event {
     @Override
     public void realitzarAccio() {
         Random rand = new Random();
+        if(getCantidad() < 6) {
         // Incrementa la cantidad entre 1 i 3 de manera aleatòria
         setCantidad(getCantidad() + (rand.nextInt(3) + 1));
+        }else {
+        	System.out.println("Ja tens massa boles de neu!! ");
+        }
     }
 }
