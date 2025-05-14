5b8fc37d4ebfb8dc3f9f7ac50b62b4c05b640cb1
LILLK <oriolnieto2006@gmail.com>
2025-05-14 19:00:32 +0200

    added content to MotoDeNeu

--- src/model/MotoDeNeu.java
+++ src/model/MotoDeNeu.java
@@ -1,5 +1,25 @@
 package model;
 
-public class MotoDeNeu {
+import java.util.ArrayList;
 
+public class MotoDeNeu extends Casella{
+
+	public MotoDeNeu(int posicio, ArrayList<Jugador> jugadorsActuals) {
+		super(posicio, jugadorsActuals);
+		
+	}
+	
+
+	@Override
+	public void realitzarAccio() {
+		
+		/*
+		for(int i = super.getPosicio()+1; 50 > i; i++) {
+			if(Taulell.getCasillas[i] instanceof Trineu) {
+				for(Jugador j : super.jugadorsActuals) {
+					j.setPosicio(i);
+				}
+			}
+		}*/
+	}
 }
